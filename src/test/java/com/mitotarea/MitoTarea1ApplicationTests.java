885407cdfb6be5d87f4610728829b0bd2cec03f2
Vicente_Mori <vomoro96@gmail.com>
2020-09-05 10:16:14 -0500

    Primer Commit

---
+++ src/test/java/com/mitotarea/MitoTarea1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitotarea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MitoTarea1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
